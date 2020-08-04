771dd94fddd584914a110baa6206911d4f6d5acd
Wenjun Ge <gew1@ornl.gov>
2020-08-04 12:18:18 -0400

    add build script for kokkos

---
+++ scripts/installKokkos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+KOKKOS_VERSION=3.1.00
+KOKKOS_OPTION="-DKokkos_ENABLE_SERIAL=ON -DKokkos_ENABLE_OPENMP=ON -DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_LAMBDA=ON"
+KOKKOS_DIR=/opt/sw/kokkos.${KOKKOS_VERSION}
+wget --quiet https://github.com/kokkos/kokkos/archive/${KOKKOS_VERSION}.tar.gz
+mkdir -p kokkos
+tar -xf ${KOKKOS_VERSION}.tar.gz -C kokkos --strip-components=1
+cd kokkos
+mkdir -p build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=${KOKKOS_DIR} -D CMAKE_CXX_COMPILER=$PWD/../bin/nvcc_wrapper ${KOKKOS_OPTIONS} ..
+make -j16 install
