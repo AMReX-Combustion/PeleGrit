c987ae58e489dcc8613051205ea21265674f675e
w0g <gew1@ornl.gov>
2020-08-25 00:01:39 -0400

    update the cuda test

--- example/CudaCode/simple.cu
+++ example/CudaCode/simple.cu
@@ -2,24 +2,28 @@
 #include <chrono>
 #include <cuda_profiler_api.h>
 
-__global__ void parallel_for(int n) {
+__global__ void parallel_for(const int n, double* da) {
   for(int i=0; i<n; i++)
   {
-    double dummy0=1.0;
-    double dummy1=dummy0+23232.434*dummy0;
+    double dummy = 123.456;
+    da[i] = dummy + 123.456*dummy;
   }
 }
 
 int main()
 {
-  const int N = 460000000;
+  const int N = 460000;
   int blockSize = 256;
   int numBlocks = (N + blockSize -1) / blockSize;
 
+  double* da;
+
+  cudaMalloc((void**)&da, sizeof(double)*N);
+
   //warm up
-  for(int j=0; j<100; j++)
+  for(int j=0; j<10; j++)
   {
-    parallel_for<<<numBlocks, blockSize>>>(N);
+    parallel_for<<<numBlocks, blockSize>>>(N, da);
   }
 
   typedef std::chrono::high_resolution_clock Time;
@@ -32,7 +36,7 @@ int main()
 
   for(int j=0; j<10; j++)
   {
-    parallel_for<<<numBlocks, blockSize>>>(460000000);
+    parallel_for<<<numBlocks, blockSize>>>(N, da);
   }
   
   cudaDeviceSynchronize();
@@ -42,6 +46,8 @@ int main()
   auto finish_clock = Time::now();
   fsec fs = finish_clock - start_clock;
   std::cout << "time taken for cuda parallel fro (msecs):" << fs.count()*1e3 << std::endl;
+
+  cudaFree(da);
 
   return 0; 
 }
